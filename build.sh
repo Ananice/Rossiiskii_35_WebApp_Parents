bed6403fddb5ccf8cbdb2f772f0dfffcd4e93e96
Российский Даниил Ильич <darkdanyacr7@gmail.com>
2026-02-09 12:18:59 +0300

    feat(deploy): настроить Render и демо-аккаунты

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
+set -e
+
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py migrate
+
+if [ "${AUTO_CREATE_DEMO_USERS:-0}" = "1" ]; then
+  python manage.py create_demo_users || true
+fi
